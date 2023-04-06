3191d82114f9717ba3304578b14eb84c6a69a26f
Volker Roth <voker.roth@mbgr.de>
2023-04-06 16:44:30 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use std::net::SocketAddr;
+
+use axum::{
+    extract::ConnectInfo,
+    routing::{get, post},
+    Json, Router,
+};
+use tower_http::trace::TraceLayer;
+// use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+/// Having a function that produces our app makes it easy to call it from tests
+/// without having to create an HTTP server.
+#[allow(dead_code)]
+fn app() -> Router {
+    Router::new()
+        .route("/", get(|| async { "Hello, World!" }))
+        .route(
+            "/json",
+            post(|payload: Json<serde_json::Value>| async move {
+                Json(serde_json::json!({ "data": payload.0 }))
+            }),
+        )
+        .route(
+            "/requires-connect-into",
+            get(|ConnectInfo(addr): ConnectInfo<SocketAddr>| async move { format!("Hi {addr}") }),
+        )
+        // We can still add middleware
+        .layer(TraceLayer::new_for_http())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        extract::connect_info::MockConnectInfo,
+        http::{self, Request, StatusCode},
+    };
+    use serde_json::{json, Value};
+    use std::net::{SocketAddr, TcpListener};
+    use tower::Service;
+    // for `call`
+    use tower::ServiceExt; // for `oneshot` and `ready`
+
+    #[tokio::test]
+    async fn hello_world() {
+        let app = app();
+
+        // `Router` implements `tower::Service<Request<Body>>` so we can
+        // call it like any tower service, no need to run an HTTP server.
+        let response = app
+            .oneshot(Request::builder().uri("/").body(Body::empty()).unwrap())
+            .await
+            .unwrap();
+
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
+        assert_eq!(&body[..], b"Hello, World!");
+    }
+}
+
+fn main() {}
