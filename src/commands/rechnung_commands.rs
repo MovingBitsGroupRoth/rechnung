07a818bd3f7cae0b74725a62e40ae5fc296dba05
Volker Roth <voker.roth@mbgr.de>
2023-05-03 06:45:41 +0200

    RE-0002 - Lifetimes removed

--- src/commands/rechnung_commands.rs
+++ src/commands/rechnung_commands.rs
@@ -8,7 +8,7 @@ pub struct ErstelleRechnung<'a> {
     pub(crate) betrag: Money<'a, Currency>
 }
 
-pub fn build_erstelle_rechnung<'a>(rechnungs_nummer: String, rechnungs_datum: Option<NaiveDate>, betrag: Money<'a, Currency>) -> ErstelleRechnung {
+pub fn build_erstelle_rechnung(rechnungs_nummer: String, rechnungs_datum: Option<NaiveDate>, betrag: Money<Currency>) -> ErstelleRechnung {
     ErstelleRechnung {
         rechnungs_nummer,
         rechnungs_datum,
